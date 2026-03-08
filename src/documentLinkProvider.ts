85a97d294d1e401dd5c2b95a2c160640c3c240f1
Dan Davison <dandavison7@gmail.com>
2026-03-08 10:20:04 -0400

    Add hover tooltip showing URL target on document links

--- src/documentLinkProvider.ts
+++ src/documentLinkProvider.ts
@@ -22,7 +22,11 @@ export class MarkdownDocumentLinkProvider implements vscode.DocumentLinkProvider
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		token: vscode.CancellationToken,
 	): vscode.ProviderResult<vscode.DocumentLink[]> {
-		return this.links.map((data) => new vscode.DocumentLink(data.range, vscode.Uri.parse(data.target)));
+		return this.links.map((data) => {
+			const link = new vscode.DocumentLink(data.range, vscode.Uri.parse(data.target));
+			link.tooltip = data.target;
+			return link;
+		});
 	}
 
 	triggerUpdate() {
